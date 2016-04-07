7a059bf855ecceff2707d3b4708924afc5b31889
Delphine Foo-Matkin <delphinef@gmail.com>
2016-04-07 09:26:52 -0700

    Completed ex4.

---
+++ day1-pristine/exercises/ex4/ex4-oloo.js
@@ -0,0 +1,44 @@
+var Widget = {
+  render: function($where) {
+    if (this.$elem) {
+      this.$elem.css({
+        width: this.width + "px",
+        height: this.height + "px"
+      }).appendTo($where);
+    }
+  },
+
+  init: function(width, height) {
+    this.width = width || 50;
+    this.height = height || 50;
+    this.$elem = null;
+  }
+};
+
+var Button = Object.create(Widget);
+
+Button.setup = function(width, height, label) {
+  this.init(width, height);
+  this.label = label || "Default";
+  this.$elem = $("<button>").text(this.label);
+};
+
+Button.build = function($where) {
+  this.render($where);
+  this.$elem.click(this.onClick.bind(this));
+};
+
+Button.onClick = function(evt) {
+  console.log("Button " + this.label + " clicked!");
+};
+
+$(document).ready(function(){
+  var $body = $(document.body);
+  var btn1 = Object.create(Button);
+  var btn2 = Object.create(Button);
+  btn1.setup(25, 25, "First");
+  btn2.setup(75, 50, "Second");
+
+  btn1.build($body);
+  btn2.build($body);
+});
--- day1-pristine/exercises/ex4/ex4.js
+++ day1-pristine/exercises/ex4/ex4.js
@@ -13,29 +13,33 @@ Widget.prototype.render = function($where){
 	}
 };
 
-function Button(/* ... */) {
-/*
-	...
+function Button(width, height, label) {
+	Widget.call(width, height, label);
+	this.label = label;
 	this.$elem = $("<button>").text(this.label);
-*/
+	this.width = width;
+	this.height = height;
 }
 
-/*
-Button -> render = function($where) {
+Button.prototype = Object.create(Widget.prototype);
+
+Button.prototype.render = function($where) {
+	var self = this;
 	// call the parent render()
+	Widget.prototype.render.call(self, $where);
 	// add a click handler -> onClick
+	this.$elem.click(this.onClick.bind(this));
 }
 
-Button -> onClick = function(evt) {
-	console.log("...");
+Button.prototype.onClick = function(evt) {
+	console.log("Button " + this.label + " clicked!");
 }
 
 $(document).ready(function(){
 	var $body = $(document.body);
-	var btn1 = ...;
-	var btn2 = ...;
+	var btn1 = new Button(25, 25, "First");
+	var btn2 = new Button(75, 50, "Second");
 
 	btn1.render($body);
 	btn2.render($body);
 });
-*/
